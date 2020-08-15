c2dd7ba9e5257e9941154f8200c39ed5777d7196
Alok701 <alokyadav1431@gmail.com>
2020-08-15 16:54:50 +0530

    Todays postion table data grid

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CustomizedTables from './components/TableDataGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomizedTables />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TableDataGrid/index.js
@@ -0,0 +1,258 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: 'lightgray',
+      color: 'black',
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+
+function createData(symbol, avg_cost, ltp, current_val, pnl, daychange, execute) {
+  return { symbol, avg_cost, ltp, current_val, pnl, daychange, execute };
+}
+
+const rows = [
+  createData('Infosys', 305, 370, 67, 4.3, 4),
+  createData('Wipro', 452, 205, 51, 4.9, 7.5),
+  createData('TCS', 262, 160, 24, 6.0, 9.2),
+  createData('HSBC', 159, 600, 24, 4.0,4.6),
+  createData('Bajaj', 356, 125, 49, 3.9, 5.5),
+  createData('TVS', 408, 302, 87, 6.5, 7.25),
+  createData('Hero', 237, 230, 37, 4.3, 5.4),
+  createData('HDFC', 375, 150, 94, 0.0, 3.4),
+  createData('KOTAK', 518, 226.0, 65, 7.0, 7),
+  createData('L&T', 392, 142, 98, 0.0, 12.2),
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  { id: 'symbol', numeric: false, disablePadding: true, label: 'Symbol' },
+  { id: 'avg_cost', numeric: true, disablePadding: false, label: 'Avg Cost' },
+  { id: 'ltp', numeric: true, disablePadding: false, label: 'LTP' },
+  { id: 'current_val', numeric: true, disablePadding: false, label: 'Current Value' },
+  { id: 'pnl', numeric: true, disablePadding: false, label: 'P&L' },
+  { id: 'daychange', numeric: true, disablePadding: false, label: 'Day Change' },
+  { id: 'execute', numeric: true, disablePadding: false, label: 'Execute' },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <span style={{fontSize:'30px', float:'left', color:'gray'}}>Today's Position</span>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <StyledTableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </StyledTableCell>
+        ))}
+      </TableRow>
+      
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '95%',
+    padding: '50px',
+  },
+  paper: {
+    width: '95%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleClick = (event, name) => {
+    
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <StyledTableRow
+                      onClick={(event) => handleClick(event, row.name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.symbol}
+                      </TableCell>
+                      <TableCell align="right">{row.avg_cost}</TableCell>
+                      <TableCell align="right">{row.ltp}</TableCell>
+                      <TableCell align="right">{row.current_val}</TableCell>
+                      <TableCell align="right">{row.pnl}</TableCell>
+                      <TableCell align="right">{row.daychange}%</TableCell>
+                      <TableCell align="right">
+                      <Button variant="contained" color="primary">Click to Execute</Button>
+                      </TableCell>
+                    </StyledTableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+}
